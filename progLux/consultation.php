cdff0783a88bc799f2641af4e67e067191babc91
Asen12345 <58296163+Asen12345@users.noreply.github.com>
2020-08-09 16:08:53 +0300

    Add files via upload

---
+++ progLux/consultation.php
@@ -0,0 +1,35 @@
+<?php
+
+if($_POST['name'] == ''){
+
+$_POST['status'] = 1;
+print_r($_POST['status']);
+
+
+}
+else if($_POST['number'] == ''){
+
+    $_POST['status'] = 2;
+    print_r($_POST['status']);
+    
+    
+    }
+
+    else if($_POST['number'] != '' && $_POST['name'] != ''){
+  
+
+        $_POST['status'] = 3;
+        print_r($_POST['status']);
+
+        $title = 'Сообщение с сайта';
+        $mess =  'Отправлено сообщение из блока "Бесплатная констультация". Номер: ' . $_POST['number'] . ', Имя: ' . $_POST['name'] . ', Комментарии: ' . $_POST['message'];
+        $to =  'andrey_yagin@mail.ru';
+   
+        // функция, которая отправляет наше письмо.
+    // mail($to, $title, $mess, 'From:'.$from);
+    mail("andrey_yagin@mail.ru", $title, $mess);
+       
+
+    }
+
+?>
